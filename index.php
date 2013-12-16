b95149306bf63799ede750ab7e6cea0013e9a6d6
DavidBITB <b00017553@student.itb.ie>
2013-12-16 15:25:22 +0000

    First Draft of Template

---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Priceless Music Store</title>
+		<link href="css/main.css" rel="stylesheet" type="text/css" />
+
+		<script type="text/javascript" src="js/main.js"></script>
+
+	</head>
+	<body>
+		<div id="wrapper">
+			<div id="header">
+
+			<h1>Priceless Music</h1>
+
+
+
+
+
+			</div>
+			<div class="clear"></div>
+			<div id="nav">
+				<ul id="menu" class="menu">
+					<li>
+						<a href="index.php">
+							<h2>
+							Home
+							</h2>
+							
+						</a>
+					</li>
+					<li>
+						<a href="browse.php">
+							<h2>
+							Browse
+							</h2>
+							
+						</a>
+					</li>
+					<li>
+						<a href="forum.php">
+							<h2>
+							Forum
+							</h2>
+							
+						</a>
+					</li>
+					<li>
+						<a href="profile.php">
+							<h2>
+							Profile
+							</h2>
+							
+						</a>
+					</li>
+				</ul>
+			</div>
+			<div class="clear"></div>
+			<div id="content">
+				<div id="threeSections">
+					<div id="left">
+						<h2>Popular Comments</h2>
+
+					</div>
+					<div id="middle">
+						image slider displaying offers goes here
+
+
+					</div>
+					<div id="right">
+						<h2>
+							New Releases
+						</h2>
+					</div>
+				</div>
+			</div>
+			<div class="clear"></div>
+			<div id="chart">This is the chart</div>
+			<div class="clear"></div>
+			<div id="footer">
+				<div id="lFooter">
+				l
+				</div>
+				<div id="mFooter">
+				m
+				</div>
+				<div id="rFooter">
+				r
+				</div>
+
+			</div>
+		</div>
+	</body>
+</html>
